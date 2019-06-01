33e504e1c8ec1f8bb228c7d30fcda175f8a47fc8
Sahan Nagodavithana <sahan@MacBook-Pro.local>
2019-06-01 14:14:58 +0530

    User Detai Genarate OK

--- hackthon/lib/views/profile_tab.dart
+++ hackthon/lib/views/profile_tab.dart
@@ -12,21 +12,45 @@ class ProfileTab extends StatefulWidget {
 }
 
 class _ProfileTabState extends State<ProfileTab> {
+  FirebaseUser user;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  doAsyncStuff() async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    setState(() {
+      this.user = user;
+    });
+  }
+
+  Future<FirebaseUser> _getCurrentUserName() async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    return user;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Container(
-          child: Text(
-            'profile',
-          ),
-        ),
-        FlatButton(
-            child: Text("Sign Out"),
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
+            child: FutureBuilder(
+            future: _getCurrentUserName(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData)
+                return new Text(snapshot.data.toString());
+              else
+                return new Container();
             },
-          )
+        )),
+        FlatButton(
+          child: Text("Sign Out"),
+          onPressed: () {
+            FirebaseAuth.instance.signOut();
+          },
+        )
       ],
     );
   }
