2cfbddc17fc64e3a31d741bba9a34d3036f91087
Avarjana <acpasavarjana@gmail.com>
2019-06-01 13:48:56 +0530

    storage

--- hackthon/lib/views/home_tab_pages/posts.dart
+++ hackthon/lib/views/home_tab_pages/posts.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'package:firebase_storage/firebase_storage.dart';
 
 class PostsPage extends StatefulWidget {
   @override
@@ -10,12 +15,19 @@ class _PostsPageState extends State<PostsPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: MaterialButton(
-      onPressed: _addPost(),
-    ));
+      child: MaterialButton(
+        onPressed: _addPost,
+        child: Text('lol'),
+      ),
+    );
   }
 
   _addPost() async {
-    Firestore.instance.collection('books').document().setData({'title': 'title', 'author': 'author'});
+    await Firestore.instance.collection('posts').document('1').setData({'title': 'title', 'author': 'author'});
+
+    File imageFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+    StorageReference ref = FirebaseStorage.instance.ref().child('someidhere').child("image.jpg");
+    StorageUploadTask uploadTask = ref.putFile(imageFile);
+    return await (await uploadTask.onComplete).ref.getDownloadURL();
   }
 }
