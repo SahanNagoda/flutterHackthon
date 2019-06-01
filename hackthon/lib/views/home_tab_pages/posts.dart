cbcaa99077bf3e33a0fa8d3f2a38ae36ee85fdf6
Avarjana <acpasavarjana@gmail.com>
2019-06-01 13:08:51 +0530

    com

--- hackthon/lib/views/home_tab_pages/posts.dart
+++ hackthon/lib/views/home_tab_pages/posts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PostsPage extends StatefulWidget {
   @override
@@ -8,6 +9,13 @@ class PostsPage extends StatefulWidget {
 class _PostsPageState extends State<PostsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('posts'));
+    return Container(
+        child: MaterialButton(
+      onPressed: _addPost(),
+    ));
+  }
+
+  _addPost() async {
+    Firestore.instance.collection('books').document().setData({'title': 'title', 'author': 'author'});
   }
 }
